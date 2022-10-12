79ce6f38207df13f2852cd594849d8b06ff8c0cf
gonzakayunawel <go.cayunao@gmail.com>
2022-10-12 15:06:22 -0300

    Cambio en el nombre de los botones

--- my_app.py
+++ my_app.py
@@ -7,15 +7,15 @@ ventana.title("My Bot App")
 ventana.iconbitmap("./hola.ico")
 
 # Crea 1er botón
-boton0 = tkinter.Button(ventana, text = "ABRE PAINT", command = my_first_bot.abrepaint)
-boton0.pack()
+boton_paint = tkinter.Button(ventana, text = "ABRE PAINT", command = my_first_bot.abrepaint)
+boton_paint.pack()
 
 # Crea 2do botón
-boton1 = tkinter.Button(ventana, text = "ABRE YOUTUBE", command = my_first_bot.abreyoutube)
-boton1.pack()
+boton_youtube = tkinter.Button(ventana, text = "ABRE YOUTUBE", command = my_first_bot.abreyoutube)
+boton_youtube.pack()
 
-boton2 = tkinter.Button(ventana, text = "ABRE Documento de Google", command = my_first_bot.documentogoogle)
-boton2.pack()
+boton_googledocs = tkinter.Button(ventana, text = "ABRE Documento de Google", command = my_first_bot.documentogoogle)
+boton_googledocs.pack()
 
 # Loop de ejecución
 ventana.mainloop()
